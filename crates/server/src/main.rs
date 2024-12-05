56dbaa9b4dbbbc4491e8b154db886515d6503a71
Nick Sikkema <contact@nicksikkema.com>
2024-12-04 19:25:34 -0600

    feat: add basic web server
    Use Axum to create a basic web server.

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
