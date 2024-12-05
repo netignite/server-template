67eab23d8fb1a6aae0a975cdfbb8a1f2bcc78f38
Nick Sikkema <contact@nicksikkema.com>
2024-12-04 19:26:03 -0600

    feat: add basic health api
    Add `/api/health/live` and `/api/health/ready` endpoints to the web server.

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,12 +1,13 @@
-use axum::{routing::get, Router};
+use api_router::api_router;
+use axum::Router;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let router = Router::new().nest_service("/api", api_router());
 
     // run it with hyper on localhost:3000
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
